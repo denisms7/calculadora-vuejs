6f2207e87d1dd48602d002bf014a53e0a35e7bac
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-04-12 20:12:12 -0300

    added calculos concluidos

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,33 @@
 <script setup>
+import { reactive } from 'vue';
+
+const estado = reactive({
+  num1: parseFloat(0),
+  num2: parseFloat(0),
+  operador: '+',
+  resultado: 0,
+
+});
+
+function calculo() {
+    switch (estado.operador) {
+      case '+':
+        return estado.resultado = parseFloat(estado.num1) + parseFloat(estado.num2);
+
+      case '-':
+        return estado.resultado = parseFloat(estado.num1) - parseFloat(estado.num2);
+
+      case '*':
+        return estado.resultado = parseFloat(estado.num1) * parseFloat(estado.num2);
+
+      case '/':
+        return estado.resultado = parseFloat(estado.num1) / parseFloat(estado.num2);
+
+      default:
+        return estado.resultado = 0;
+    }
+
+}
 
 
 </script>
@@ -6,12 +35,46 @@
 
 
 <template>
+  <div class="container ">
+    <header class="my-5">
+      <h1>Calculadora Aritmética</h1>
+      <p>Projeto utilizando VueJS</p>
+    </header>
 
+    <section>
+      <div class="row">
 
+        <div class="col-md-2">
+          <label for="num11" class="form-label">Numero 1:</label>
+          <input @keyup="evento => { estado.num1 = evento.target.value; calculo(); }" type="text" placeholder="0" class="form-control" id="num11">
+        </div>
+
+        <div class="col-md-1">
+          <label for="operacao" class="form-label">Operador:</label>
+          <select @change="evento => {estado.operador = evento.target.value; calculo()}" class="form-select" id="operacao">
+            <option selected value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
+        </div>
+
+        <div class="col-md-2">
+          <label for="num22" class="form-label">Numero 2:</label>
+          <input @keyup="evento => {estado.num2 = evento.target.value; calculo()}" type="text" placeholder="0" class="form-control" id="num22">
+        </div>
+
+        <div class="col-md-2">
+          <label for="resultado" class="form-label">Resultado</label>
+          <div class="form-control" id="resultado">{{ estado.resultado }}</div>
+        </div>
+
+      </div>
+    </section>
+
+  </div>
 </template>
 
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
